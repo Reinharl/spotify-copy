6acf459e7aa751806f202af4e2aee18026a8f4b1
gabrielelirangi <gabrieleelirangi@gmail.com>
2023-12-15 18:05:58 +0100

    aggiunta form

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 
 
+
 function App() {
   
     const router = createBrowserRouter([
         {path: "/", element: <Home/>},
         {path: "/search", element: <Search/>}
+        
     ]);
 
     return (
